231a63277dcd4ed24a0e967490926629fbb837bd
Kentangz <adhitsholawat12@gmail.com>
2024-11-06 23:09:00 +0700

    added js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// JavaScript untuk toggle menu
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});

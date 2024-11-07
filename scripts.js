ed0a76f70d41479908c5f117f3d5eab130d7ec30
Kentangz <adhitsholawat12@gmail.com>
2024-11-07 23:53:36 +0700

    delete js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,2 @@
 // JavaScript untuk toggle menu
-const menuToggle = document.querySelector(".menu-toggle");
-const navbar = document.querySelector(".navbar");
-
-menuToggle.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
+ 
